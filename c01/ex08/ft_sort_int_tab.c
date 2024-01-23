a760db48b1612fa2cccb3e9b4c2817ea345c815e
Andre David Aires De Freitas <aaires-d@c1r9s15.42porto.com>
2024-01-23 01:35:34 +0000

    added C01

---
+++ c01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaires-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 10:11:03 by aaires-d          #+#    #+#             */
+/*   Updated: 2024/01/11 10:11:10 by aaires-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	swap;
+	int	count;
+
+	count = 0;
+	while (count < (size - 1))
+	{
+		if (tab[count] > tab[count + 1])
+		{
+			swap = tab[count];
+			tab[count] = tab[count + 1];
+			tab[count + 1] = swap;
+			count = 0;
+		}
+		else
+			count++;
+	}
+}
